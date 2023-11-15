fd4cf783642540cebd7f2216d709a3c0526e0b63
Anderskeller <anderskeller00@gmail.com>
2023-11-15 11:14:08 +0100

    Json fil for overAllData og usaData

---
+++ data.js
@@ -0,0 +1,27 @@
+let dataJson = `[
+   {
+      "GenreName":"Rock",
+      "TotalRevenue":826.65
+   },
+   {
+      "GenreName":"Latin",
+      "TotalRevenue":382.14
+   },
+   {
+      "GenreName":"Metal",
+      "TotalRevenue":261.36
+   },
+   {
+      "GenreName":"Alternative & Punk",
+      "TotalRevenue":241.56
+   },
+   {
+      "GenreName":"TV Shows",
+      "TotalRevenue":93.53
+   },
+   {
+      "GenreName":"Jazz",
+      "TotalRevenue":79.20
+   }
+]`
+let data = JSON.parse(dataJson)
